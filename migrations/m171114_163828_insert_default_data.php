ba77870c6448d08d708deab172f6384e5c0e6269
Ilya Agafonov <tairesh.rus@gmail.com>
2017-11-14 21:47:37 +0500

    default data

---
+++ migrations/m171114_163828_insert_default_data.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m171114_163828_insert_default_data
+ */
+class m171114_163828_insert_default_data extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $this->batchInsert('Authors', ['name'], [
+            ["CrazyNews"],
+            ["Чук и Гек"],
+            ["CatFuns"],
+            ["CarDriver"],
+            ["BestPics"],
+            ["ЗОЖ"],
+            ["Вася Пупкин"],
+            ["Готовим со вкусом"],
+            ["Шахтёрская Правда"],
+            ["FunScience"],
+        ]);
+        $this->batchInsert('Languages', ['name'], [
+            ["Русский"],
+            ["English"],
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->truncateTable('Authors');
+        $this->truncateTable('Languages');
+    }
+}
