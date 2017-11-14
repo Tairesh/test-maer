149d969ebc697f27f4e750eb039d75ab70481613
Ilya Agafonov <tairesh.rus@gmail.com>
2017-11-14 20:55:05 +0500

    init

---
+++ commands/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\commands;
+
+use yii\console\Controller;
+
+/**
+ * Test
+ */
+class TestController extends Controller
+{
+    
+    public function actionIndex()
+    {
+        
+    }
+    
+}
