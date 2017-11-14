0dce57f074d4b81da1755f335b1affef6d359e44
Ilya Agafonov <tairesh.rus@gmail.com>
2017-11-14 21:37:53 +0500

    created CRUD

---
+++ views/post/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Post */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="post-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'languageId')->textInput() ?>
+
+    <?= $form->field($model, 'authorId')->textInput() ?>
+
+    <?= $form->field($model, 'dateCreated')->textInput() ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'likesCount')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
